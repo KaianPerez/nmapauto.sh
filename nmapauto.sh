bf573a597bc2e8667d572a24229347cfa0b8f399
Kaian Pérez <kaianperez@gmail.com>
2022-11-25 15:39:34 +0100

    Actualizando cosas

--- nmapauto.sh
+++ nmapauto.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 #Colores
-greenColour="\e[0;32m\033[1m"
-redColour="\e[0;31m\033[1m"
-endColour="\033[0m\e[0m"
+verde="\e[0;32m\033[1m"
+rojo="\e[0;31m\033[1m"
+fincolor="\033[0m\e[0m"
 
 function ctrl_c(){
-    echo -e "\n ${redColour}[*] Saliendo del programa${endColour}"
+    echo -e "\n ${rojo}[*] Saliendo del programa${fincolor}"
     exit 1
 }
 
@@ -16,23 +16,24 @@ trap ctrl_c INT
 # Comprobación de argumentos
     if [ $# -eq 1 ]; then
         if [[ "$1" =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$ ]]; then
-            echo -e "\n ${greenColour}[*] Reconocimiento inicial de puertos${endColour}\n"
+            echo -e "\n ${verde}[*] Reconocimiento inicial de puertos${fincolor}\n"
             ip=$1
             nmap -p- --open -T5 -v -n $ip -oG ports.tmp
         else
-            echo -e "\n ${redColour}[*] Introduce una IPv4 correcta${endColour}\n" 
+            echo -e "\n ${rojo}[*] Introduce una IPv4 correcta${fincolor}\n" 
             exit 1
         fi
     else
-        echo -e "\n ${redColour}[*] Introduce solamente la IP a escanear${endColour}\n"
+        echo -e "\n ${rojo}[*] Introduce solamente la IP a escanear${fincolor}\n"
         exit 1
     fi
 
 # Escaneo de puertos
     ports="$(cat ports.tmp | grep -oP '\d{1,5}/open' | awk '{print $1}' FS='/' | xargs | tr ' ' ',')"
-    echo -e "\t[*] Dirección IP: $ip" > ListaPuertos
-    echo -e "\t[*] Puertos abiertos: $ports\n" >> ListaPuertos
-    echo -e "\n ${greenColour}[*] Escaneo avanzado de servicios\n${endColour}" 
+    echo -e "\n ${verde}[*] Escaneo avanzado de servicios\n${fincolor}" 
     nmap -sCV -p$ports $ip -oN InfoPuertos
+    sed -i '1,3d' InfoPuertos
+    echo -e "\n \t[*] Dirección IP: $ip" >> InfoPuertos
+    echo -e "\t[*] Puertos abiertos: $ports\n" >> InfoPuertos
     rm ports.tmp
-    echo -e "\n ${greenColour}[*] Escaneo completado, se han generado los ficheros ListaPuertos e InfoPuertos\n${endColour}" 
+    echo -e "\n ${verde}[*] Escaneo completado, se ha generado el fichero InfoPuertos\n${fincolor}" 
