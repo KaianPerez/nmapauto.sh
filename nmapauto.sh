647a17aefdf1ef69a673c3f2a2bd014c6c0f5bb5
Kaian Pérez <113518519+KaianPerez@users.noreply.github.com>
2023-02-23 12:50:04 +0100

    Update nmapauto.sh

--- nmapauto.sh
+++ nmapauto.sh
@@ -6,7 +6,7 @@ rojo="\e[0;31m\033[1m"
 fincolor="\033[0m\e[0m"
 
 function ctrl_c(){
-    echo -e "\n ${rojo}[*] Saliendo del programa${fincolor}"
+    echo -e "\n ${rojo}[*] Saliendo del programa \n${fincolor}"
     exit 1
 }
 
@@ -15,20 +15,20 @@ trap ctrl_c INT
 
 # Comprobación de argumentos
     if ! [ $(id -u) = 0 ]; then 
-	echo -e "\n ${rojo}[*] Debes utilizar sudo o ser root ${fincolor}\n"
+	echo -e "\n ${rojo}[*] Debes utilizar sudo o ser root \n${fincolor}"
 	exit 1 
     fi
     if [ $# -eq 1 ]; then
         if [[ "$1" =~ ^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$ ]]; then
-            echo -e "\n ${verde}[*] Reconocimiento inicial de puertos${fincolor}\n"
+            echo -e "\n ${verde}[*] Reconocimiento inicial de puertos \n${fincolor}"
             ip=$1
             nmap -p- -sS --min-rate 5000 --open -Pn -v -n $ip -oG ports.tmp
         else
-            echo -e "\n ${rojo}[*] Introduce una IPv4 correcta${fincolor}\n" 
+            echo -e "\n ${rojo}[*] Introduce una IPv4 correcta \n${fincolor}" 
             exit 1
         fi
     elif [ $# -eq 0 ]; then
-		ip a | grep "tun0" &>/dev/null && echo -e "\n ${rojo}[*] Estás bajo una VPN, introduce una IPv4 correcta ${fincolor}\n" && exit 1
+		ip a | grep "tun0" &>/dev/null && echo -e "\n ${rojo}[*] Estás bajo una VPN, introduce una IPv4 correcta \n${fincolor}" && exit 1
 		ip=$(sudo arp-scan -l | grep "PCS" | grep -v $(hostname -I) | cut -f1 | tail -n1)
 			if [[ $ip = "" ]]; then
 			echo -e "\n ${rojo}[*] No se ha detectado ninguna IP de la máquina víctima ${fincolor}\n"
@@ -43,11 +43,15 @@ trap ctrl_c INT
 
 # Escaneo de puertos
     ports="$(cat ports.tmp | grep -oP '\d{1,5}/open' | awk '{print $1}' FS='/' | xargs | tr ' ' ',')"
+    		if [[ $ports = "" ]]; then
+			echo -e "\n ${rojo}[*] No se ha detectado ningún puerto abierto de la máquina víctima \n${fincolor}"
+			exit 1
+			fi
     echo -e "\n ${verde}[*] Escaneo avanzado de servicios\n${fincolor}" 
     nmap -sCV -p$ports $ip -oN InfoPuertos
     sed -i '1,3d' InfoPuertos
     echo -e "\n \t[*] Dirección IP: $ip" >> InfoPuertos
     echo -e "\t[*] Puertos abiertos: $ports\n" >> InfoPuertos
     rm ports.tmp
-    echo -e "\n ${verde}[*] Escaneo completado, se ha generado el fichero InfoPuertos\n${fincolor}" 
+    echo -e "\n ${verde}[*] Escaneo completado, se ha generado el fichero InfoPuertos \n${fincolor}" 
     echo $ip | xclip -sel clip 
